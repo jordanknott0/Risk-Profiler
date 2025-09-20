12d8a326e7d4cc9c13e141cd0106581505b0b9d9
jordanknott0 <jordanknott0@gmail.com>
2025-09-20 20:29:03 +0100

    Add files via upload

---
+++ wrapper component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import JointRiskProfiler from "./JointRiskProfiler";
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", margin: "20px" }}>
+      <h1>Joint Investment Risk Profiler</h1>
+      <JointRiskProfiler />
+    </div>
+  );
+}
